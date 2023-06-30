29eea0cd3d0dc2671080dc53ecd3777ee0a31a96
Yashwanth Reddy <yashwanth@renlabs.org>
2023-06-30 17:35:02 +0530

    fix: config update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,34 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
+import "@typechain/hardhat";
+import "hardhat-deploy";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+    solidity: {
+        settings: {
+            //   viaIR: true,
+            optimizer: {
+                enabled: true,
+                // runs: 100000,
+            },
+        },
+        version: "0.8.18",
+    },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+    },
+    networks: {
+        hardhat: {
+            forking: {
+                url:
+                    "https://mainnet.infura.io/v3/" +
+                    process.env.INFURA_API_KEY,
+            },
+        },
+    },
 };
 
 export default config;
